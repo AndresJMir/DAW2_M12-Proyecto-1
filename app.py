be9df7114fb739c7254cb7415903a27ca37db567
AndresJMir <anunez1@insjoaquimmir.cat>
2023-10-06 20:16:37 +0200

    Tabla list y create!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, request, abort, render_template, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -13,6 +13,7 @@ def get_db_connection():
     con.row_factory = sqlite3.Row
     return con
 
+
 @app.route('/')
 def init():
     return redirect(url_for('products_list'))
@@ -20,7 +21,48 @@ def init():
 @app.route('/products/list')
 def products_list():
     with get_db_connection() as con:
-        res = con.execute("SELECT id, title, price FROM products ORDER BY id ASC")
+        #res = con.execute("SELECT id, title, price FROM products ORDER BY id ASC")
+        res = con.execute("SELECT * FROM products ORDER BY id ASC")
         products = res.fetchall()
 
     return render_template('products/list.html', products = products)
+
+# Controlador
+@app.route('/products/create', methods=["GET", "POST"])
+def products_create():
+	if request.method == 'GET':
+		# Show form
+		return render_template('products/create.html')
+	elif request.method == 'POST':
+		# Get POST data
+		data = request.form
+		# TODO Validate data
+		# TODO Generate data
+		# TODO Save data (database insert)
+		# Redirect to list page
+
+		#Recibe del Post los datos
+		title = str(request.form['title'])
+		description = request.form['description']
+		photo = request.form['photo']
+		price = float(request.form['price'])
+		category_id = request.form['category_id']
+		seller_id = request.form['seller_id']
+		#Valida
+		if len(title) > 255 or len(title) == 0:
+			return render_template('products/create.html', "No se cumplen las condiciones")
+		else:
+			with get_db_connection() as ins:
+				inserte = """INSERT INTO products (title, description, photo, price, category_id, seller_id, created, updated) VALUES  ( ?, ?, ?, ?, ?, ?, DATETIME('now'), DATETIME('now'))"""
+				tupla = title, description, photo, price, category_id, seller_id
+				ins = ins.execute(inserte, tupla)
+				products = ins.fetchall()
+			return redirect(url_for('products_list')) 
+	else:
+		# Not found response
+		abort(404)
+
+@app.route('/products/update/<int:id>')
+def products_update(id):
+   # TODO Everything
+    return
